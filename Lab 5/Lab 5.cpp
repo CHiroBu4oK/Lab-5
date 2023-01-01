b49b00f33c43e9a4f1ac9f05f7ec05bb958b1e45
Олександр Боднарчук <bodnarchukk25@hotmail.com>
2023-01-02 01:15:40 +0200

    Add project files.

---
+++ Lab 5/Lab 5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+    srand(time(NULL));
+    int i[10][10], a[1][10] = { 1,1,1,1,1,1,1,1,1,1 }, min, cordX;
+
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            i[x][y] = rand() % 100;
+            cout << i[x][y] << " ";
+            if (x == 0 && y == 0) {
+                min = i[0][0];
+            }
+            if (min > i[x][y]) {
+                min = i[x][y];
+                cordX = x;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Min: " << min << endl << "Cord X: " << cordX + 1 << endl;
+    for (int y = 0; y < 10; y++) 
+    {
+        int *cum, *p, *p1,a[1][10]={1,1,1,1,1,1,1,1,1,1};
+        cum = &i[0][y];
+        p = &i[cordX][y];
+        p1 = &a[1][y];
+
+        *p1 = *cum;
+        *cum = *p;
+        *p = *p1;
+    }
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            cout << i[x][y] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
